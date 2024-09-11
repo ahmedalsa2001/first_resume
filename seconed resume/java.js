c3d8f3b206e51aca70e86adac0e2e4a5e978d78b
ahmedalsa2001 <142327291+ahmedalsa2001@users.noreply.github.com>
2024-09-11 23:28:00 +0300

    Add files via upload

---
+++ seconed resume/java.js
@@ -0,0 +1,14 @@
+function makesure(){
+    if(confirm("Are you sure you want to view my twitter account?")){
+      window.open("https://twitter.com/");
+    }
+  
+  }
+  
+  function downloadResume(){
+    if(confirm("Are you sure you want to download my resume?")){
+      window.print();
+        
+    }  
+  }
+  
